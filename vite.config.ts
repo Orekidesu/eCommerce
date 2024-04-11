79b61c9030fa364025960a7b98a9294dbfc5a632
Orekidesu <cmcodog10@gmail.com>
2024-04-11 17:44:24 +0800

    unta ma deploy na

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Addd
+  base: "/eCommerce/",
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
