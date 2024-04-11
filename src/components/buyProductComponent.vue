69ac01004c72a3b66ea29e292d69554c5ff5f810
Orekidesu <cmcodog10@gmail.com>
2024-04-11 16:22:18 +0800

    gg

--- src/components/buyProductComponent.vue
+++ src/components/buyProductComponent.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="w-screen px-4 sm:px-10 md:px-20 lg:px-40">
+    <!-- <div class="w-screen"> -->
     <div class="flex flex-col md:flex-row justify-evenly gap-10">
       <!-- CONTACT -->
+      <!-- FIRST COLUMN -->
       <div class="w-full">
+        <!-- <div class="w-full px-20 "> -->
         <form action="">
           <div class="flex flex-col gap-4 w-full">
             <div class="flex flex-col gap-2">
@@ -171,20 +174,110 @@
                 </label>
               </div>
 
+              <!-- Delivery (Shipping Method) -->
+
+              <div>
+                <FormField name="shipping">
+                  <FormItem>
+                    <FormLabel class="text-sm"><strong>Shipping Method</strong></FormLabel>
+                    <div class="relative">
+                      <FormControl>
+                        <Input type="text" class="bg-accent text-primary-foreground rounded-sm w-full" disabled
+                          placeholder="Free Shipping" />
+                        <div class="absolute right-2 top-1/2 transform -translate-y-1/2">
+                          <Car />
+
+                        </div>
+
+                      </FormControl>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                </FormField>
+              </div>
+
+              <!-- End Delivery (Shipping Method) -->
               <!-- END DELIVERY -->
+
             </div>
+            <!-- PAYMENT METHOD -->
             <div>
-
+              <FormField name="shipping">
+                <FormItem>
+                  <FormLabel class="text-xl"><strong>Payment</strong></FormLabel>
+                  <FormControl>
+                    <RadioGroup v-model="selectedValue">
+                      <div class="border p-4 rounded m-0 relative"
+                        :class="{ 'bg-accent text-accent-foreground': selectedValue === 'default' }"
+                        @click="selectValue('default')">
+                        <div class="flex items-center space-x-2">
+                          <RadioGroupItem id="r1" value="default" />
+                          <Label for="r1">Via GCash</Label>
+                          <div class="absolute right-2 top-1/2 transform -translate-y-1/2">
+                            <img src="../assets/gcash.svg" alt="gcash icon" class="h-6 w-6" style="color: #0079f6;" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="border p-4 rounded m-0 relative "
+                        :class="{ 'bg-accent text-accent-foreground': selectedValue === 'comfortable' }"
+                        @click="selectValue('comfortable')">
+                        <div class="flex items-center space-x-2">
+                          <RadioGroupItem id="r2" value="comfortable" />
+                          <Label for="r2">Cash On Delivery (COD)</Label>
+                          <div class="absolute right-2 top-1/2 transform -translate-y-1/2">
+                            <HandCoins />
+                          </div>
+                        </div>
+                      </div>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              </FormField>
             </div>
-
+            <!-- END PAYMENT METHOD -->
+            <!-- CHECKOUT -->
+            <div>
+              <div class="flex justify-start w-full">
+                <button
+                  class=" w-full border border-primary bg-secondary text-secondary-foreground py-2 rounded-sm hover:bg-primary hover:text-primary-foreground">
+                  Complete Order</button>
+              </div>
+            </div>
+            <!-- END CHECKOUT -->
           </div>
-          <div>
 
-          </div>
         </form>
       </div>
-      <div class="w-full">
+      <!-- END FIRST COLUMN -->
 
+      <!-- SECOND COLUMN -->
+      <div class="w-full">
+        <div class="flex flex-col">
+          <div class="flex flex-row justify-between">
+            <div class="flex flex-row justify-start gap-2">
+              <img src="../assets/cleanCollarDarkGray.png" class="h-20 w-20" alt="">
+              <div class="flex flex-col justify-start">
+                <div class="text-base">CleanCollar Men's Shirt</div>
+                <div class="text-xs">Navy / XS </div>
+              </div>
+            </div>
+            <div>₱1,500 PHP</div>
+          </div>
+          <br>
+          <div class="flex flex-row justify-between">
+            <div>Subtotal</div>
+            <div class="font-bold">₱1,500 PHP</div>
+          </div>
+          <div class="flex flex-row justify-between">
+            <div>Shipping</div>
+            <div class="font-bold">Free</div>
+          </div>
+          <div class="flex flex-row justify-between">
+            <div class="text-xl font-bold">Total</div>
+            <div class="text-xl font-bold">₱1,500 PHP</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -215,6 +308,15 @@ import {
   TooltipTrigger
 } from '@/components/ui/tooltip';
 import { HelpCircle } from 'lucide-vue-next';
+import { Car } from 'lucide-vue-next';
+import { HandCoins } from 'lucide-vue-next';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { Label } from '@/components/ui/label'
+import { ref } from 'vue';
+let selectedValue = ref('comfortable');
 
+const selectValue = (value: string) => {
+  selectedValue.value = value;
+};
 
 </script>
