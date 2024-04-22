35e42c450e599980e87510090619fdf84374be80
Orekidesu <cmcodog10@gmail.com>
2024-04-22 12:54:42 +0800

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,12 @@
+// SHADCN UI //
+
+export * as Buttom from "./ui/button";
+export * as Card from "./ui/card";
+export * as Carousel from "./ui/carousel";
+export * as Checkbox from "./ui/checkbox";
+export * as Form from "./ui/form";
+export * as Input from "./ui/input";
+export * as Label from "./ui/label";
+export * as RadioGroup from "./ui/radio-group";
+export * as Select from "./ui/select";
+export * as Tooltip from "./ui/tooltip";
